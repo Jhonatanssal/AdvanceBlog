1231c5ee16d4717f8f2cb3f3202deccb1f8c7760
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-09 22:04:33 -0500

    Add btns functionality Appear and disappear rich text form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-// This file is automatically compiled by Webpack, along with any other files
+;// This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
@@ -15,3 +15,25 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+
+document.addEventListener('turbolinks:load', () => {
+  document.addEventListener('click', () => {
+    let element = event.target.closest('.paragraph-content');
+    if (!element) return;
+
+    element.classList.add('d-none');
+    element.nextElementSibling.classList.remove('d-none');
+  })
+
+  document.addEventListener('click', () => {
+    if (!event.target.matches('.cancel')) return;
+
+    let element = event.target.closest('.paragraph-form');
+
+    element.classList.add('d-none');
+    element.previousElementSibling.classList.remove('d.none');
+  })
+})
